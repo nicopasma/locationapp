4dfe3efcf23ef05b4645a5001ec4984f9ba0e16f
nicopasma <n3pani00@students.oamk.fi>
2025-02-23 22:41:38 +0200

    Firebase config luotu. Aloittelua.

---
+++ firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAT6BPaWav5zEBGtagtjUYX0vjxKyu8i5E",
+  authDomain: "locationapp-92cf5.firebaseapp.com",
+  projectId: "locationapp-92cf5",
+  storageBucket: "locationapp-92cf5.firebasestorage.app",
+  messagingSenderId: "756453410181",
+  appId: "1:756453410181:web:e0fd1095e06801abbab0ec"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
